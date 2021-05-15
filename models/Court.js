cc9c4e9ff5248a2bfca5849af4c1f8b110d2800f
Night Chough <chicken.snuggles@gmail.com>
2021-05-15 10:31:58 -0500

    added court model

---
+++ models/Court.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Court extends Model {}
+
+Court.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        street: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true,
+            }
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        hasLights: {
+            type: DataTypes.BOOLEAN,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+)
+
+module.exports = Court
