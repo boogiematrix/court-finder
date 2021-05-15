00d176e20ff3fb79e80204c6f2bd380a4194e985
Night Chough <chicken.snuggles@gmail.com>
2021-05-15 13:20:53 -0500

    added GET all court route

--- controllers/api/court-routes.js
+++ controllers/api/court-routes.js
@@ -1,6 +1,22 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const Court = require('../../models/Court');
+const Game = require('../../models/Game')
 //GET all courts
+router.get('/', async (req, res) => {
+    try {
+        const courtData = await Court.findAll();
+        res.status(200).json(courtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 //GET one court
+
 //GET courts near me with games and playerCount
+
 //POST new court
+
 //PUT update court info
+
 //DELETE court
