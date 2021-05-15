e9bcf64b40fa470a9274423ec7ce1a00bfa29663
Night Chough <chicken.snuggles@gmail.com>
2021-05-15 13:52:24 -0500

    added PUT and GET nearby to court routes

--- controllers/api/court-routes.js
+++ controllers/api/court-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const Court = require('../../models/Court');
 const Game = require('../../models/Game')
+
 //GET all courts
 router.get('/', async (req, res) => {
     try {
@@ -11,6 +12,7 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 })
+
 //GET one court
 router.get('/:id', async (req, res) => {
     try {
@@ -27,14 +29,29 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
-//GET courts near me with games and playerCount
-router.get('/nearme', async (req, res) => {
-    try {
 
+//GET courts near me with games and playerCount
+router.get('/:zip', async (req, res) => {
+    try {
+        const courtData = await Court.findAll({
+            where: {
+                zip: req.params.zip,
+            },
+        },
+            {
+                include: [Game]
+            });
+        if (!courtData) {
+            res.status(404).json({ message: 'No courts nearby!' });
+            return;
+        }
+
+        res.status(200).json(courtData);
     } catch (err) {
         res.status(500).json(err);
     }
 })
+
 //POST new court
 router.post('/', async (req, res) => {
     try {
@@ -44,14 +61,27 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 })
+
 //PUT update court info
 router.put('/:id', async (req, res) => {
     try {
-        
+        const courtData = await Court.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!courtData[0]) {
+            res.status(404).json({ message: 'No court found with this id!' });
+            return;
+        };
+
+        res.status(200).json(courtData)
     } catch (err) {
         res.status(400).json(err);
     }
 })
+
 //DELETE court
 router.delete('/:id', async (req, res) => {
     try {
