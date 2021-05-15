7ec3bd4688e6e88ac75e98f95c345c6d82510ff5
Night Chough <chicken.snuggles@gmail.com>
2021-05-15 11:12:26 -0500

    added player through table

---
+++ models/Player.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Player extends Model {}
+
+Player.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'game',
+        key: 'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'player',
+  }
+);
+
+module.exports = Player;
