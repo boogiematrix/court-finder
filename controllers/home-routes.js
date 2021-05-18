21b61f5ae6bbf21fc861211e4bf2525c3661edda
Night Chough <chicken.snuggles@gmail.com>
2021-05-17 20:41:21 -0500

    added routes for more views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,10 +45,149 @@ router.get('/signup', (req, res) => {
     }
 });
 //GET individual court view
+router.get('/courts/:id', async (req, res) => {
+    try {
+        const courtData = await Court.findByPk(req.params.id, {
+            include: [User, Game],
+            subQuery: false,
+        });
+
+        if (postData) {
+            const court = courtData.get({ plain: true });
+    
+            res.render('onecourt', {
+                court,
+                loggedIn: req.session.loggedIn,
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
 //GET games by court view
+router.get('/games/:court_id', async (req, res) => {
+    try {
+        const gameData = await Game.findbyAll({
+            include: [Court],
+        }, {
+            where: {
+                court_id: req.params.court_id,
+            },
+        });
+
+        if (gameData) {
+            const game = gameData.get({ plain: true });
+            res.render('court-games', {
+                game,
+                loggedIn: req.session.loggedIn,
+        })
+            } else {
+                res.status(404).json({ message: 'No game found at this court!' });     
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+})
 //GET one game
+router.get('/games/:id', async (req, res) => {
+    try {
+        const gameData = await Game.findByPk(req.params.id, {
+            include: [User, Court],
+            subQuery: false,
+        });
+
+        if (gameData) {
+            const game = gameData.get({ plain: true });
+
+            res.render('onegame', {
+                game,
+                loggedIn: req.session.loggedIn,
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
 //GET user's games view
+router.get('/games/:user_id', withAuth, async (req, res) => {
+    try {
+        const gameData = await Game.findAll({
+            include:  [User],
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+        if (!gameData) {
+            res.status(404).json();
+        }
+        const game = gameData.map((game) => game.get({ plain: true }));
+        res.render('usergames', {
+            game,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+})
 //GET add court
+router.get('/newcourt', withAuth, (req, res) => {
+    res.render('newcourt', {
+        loggedIn: req.session.loggedIn,
+    });
+    return;
+})
 //GET update court
+router.get('/updatecourt/:id', withAuth, async (req, res) => {
+    try {
+        const courtData = await Court.findByPk(req.params.id);
+
+        if (courtData) {
+            const court = courtData.get({ plain: true });
+
+            res.render('updatecourt', {
+                court,
+                loggedIn: req.session.loggedIn,
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
 //GET add game
+router.get('/addgame', withAuth, (req, res) => {
+    res.render('addgame', {
+        loggedIn: req.session.loggedIn,
+    });
+    return;
+})
 //GET update game
+router.get('/updategame/:id', withAuth, async (req, res) => {
+    try {
+        const gameData = await Game.findByPk(req.params.id);
+
+        if (gameData) {
+            const game = gameData.get({ plain: true });
+
+            res.render('updategame', {
+                game,
+                loggedIn: req.session.loggedIn,
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
