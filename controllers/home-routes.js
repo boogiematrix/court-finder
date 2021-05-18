244aaf521fd312bf8d595e27812180113a3f7105
Night Chough <chicken.snuggles@gmail.com>
2021-05-17 19:18:47 -0500

    added login, signup, and home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,49 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { Game, Court, User, Player } = require('../models/')
 //GET '/' for homepage display all courts nearby
+router.get('/', async (req, res) => {
+    try {
+        const courtData = await Court.findAll({
+            include: [User],
+            subQuery: false,
+        });
+
+        if (!courtData) {
+            res.status(403).json();
+            return;
+        }
+        const court = courtData.map((court) => court.get({ plain: true }));
+        res.render('homepage', {
+            court,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    };
+
+});
 //GET '/login'
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.render('dashboard', {
+            loggedIn: req.session.loggedIn,
+        })
+    } else {
+        res.render('login')
+    }
+});
 //GET '/signup'
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.render('dashboard', {
+            loggedIn: req.session.loggedIn,
+        });
+    } else {
+        res.render('signup')
+    }
+});
 //GET individual court view
 //GET games by court view
 //GET one game
