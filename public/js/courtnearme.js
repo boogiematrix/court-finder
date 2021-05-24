1e725f9c3c8acf0b222fae8730eeeff156de2e2f
Bij <BijanEsnaashari@gmail.com>
2021-05-24 14:08:07 -0500

    courtsnearme function

--- public/js/courtnearme.js
+++ public/js/courtnearme.js
@@ -20,19 +20,25 @@ const courtdata = [
     },
   ];
 const courtAdds=courtdata[0].address+'|'+courtdata[1].address+'|'+courtdata[2].address
-const getDistance= async( originAdr, courtAdr)=> {const courtsinArea=[];
+const getDistance= async(range, originAdr, courtAdr)=> {;
     try{
-        const resp = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+originAdr+'&destinations='+courtAdr+'&key=AIzaSyAexWk-s7fGhAtV1jikHnncG5syH41GJ1E');
+        const resp = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json',
+        {params:
+            {units: 'imperial',
+            origins:originAdr,
+            destinations:courtAdr,
+            key:'AIzaSyAexWk-s7fGhAtV1jikHnncG5syH41GJ1E'}})
     
     
+        const courtsinArea=[]
+        let courts=resp.data.rows[0].elements
         
-        console.log(resp.data)
-        //console.log(courts[0].elements[0])
         
-        //for (let i=0; i<courts.length;i++)
-        //{ if
-            //(courts[i].elements[0].distance.value<range*5280)
-            //{courtsinArea.push(i); console.log(i)}}//}   
+        for (let i=0; i<courts.length;i++)
+        { if
+            (courts[i].distance.value<range*5280)
+            {courtsinArea.push(courtdata[i].address);}}  
+        return courtsinArea   
     }
   
     catch (err) {
@@ -47,5 +53,5 @@ const getDistance= async( originAdr, courtAdr)=> {const courtsinArea=[];
 //console.log (courtdata[0].name)
 
 
-getDistance('1336+Lincoln+Ave+Saint+Paul+MN', courtAdds)
-//console.log(courtsinArea)
+console.log(getDistance(2,'1336+Lincoln+Ave+Saint+Paul+MN', courtAdds))
+
